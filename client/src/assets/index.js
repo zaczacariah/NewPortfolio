e7f07fb54f3e9da46bbb34c0cca28d1e2bcf399e
zaczacariah <150572949+zaczacariah@users.noreply.github.com>
2024-09-07 16:54:51 +1000

    src assets folder

---
+++ client/src/assets/index.js
@@ -0,0 +1,78 @@
+// Importing images
+import avatar_eyeone from './avatar_eyeone_comp.png';
+import avatar_eyetwo from './avatar_eyetwo_comp.png';
+import avatar_eyeless from './avatar_eyeless_comp.png';
+import avatar_eyeholes from './avatar_eyeholes_comp.png';
+import backdrop from './backdrop.png';
+
+//app-container
+import scatter_vertical from './scatter-vertical.png';
+import scatter from './scatter.png';
+import rough_border_vertical from './rough-border-vertical.png';
+import rough_border from './rough-border.png';
+import scatter_background from './scatter-background.png';
+import button_border from './button-border.png';
+import ozar_screenshot from './ozar_screenshot.png';
+
+
+// Avatar Icons
+import html from './avatar_icons/html_new.png';
+import css from './avatar_icons/css_new.png';
+import jscript from './avatar_icons/js_new.png';
+import react from './avatar_icons/react_new.png';
+import node from './avatar_icons/node_new.png';
+import mongo from './avatar_icons/mongo_new.png';
+import graphql from './avatar_icons/graph_new.png';
+import git from './avatar_icons/git_new.png';
+
+
+// General Icons
+import gen_react from './react.png';
+import gen_node from './node.png';
+import gen_mongo from './mongo.png';
+import gen_graphql from './graphql.png';
+import gen_mysql from './mysql.png';
+import gen_heroku from './heroku.png';
+import gen_wordpress from './wordpress.png';
+
+// Monochrome Icons
+import html_mono from './avatar_icons/html_monochrome.png';
+import css_mono from './avatar_icons/css_monochrome.png';
+
+
+
+
+// Or as an object
+export const images = {
+    avatar_eyeone,
+    avatar_eyetwo,
+    avatar_eyeless,
+    avatar_eyeholes,
+    scatter_vertical,
+    scatter,
+    rough_border_vertical,
+    rough_border,
+    scatter_background,
+    button_border,
+    backdrop,
+    ozar_screenshot,
+    gen_react,
+    gen_node,
+    gen_mongo,
+    gen_graphql,
+    gen_mysql,
+    gen_heroku,
+    gen_wordpress,
+    html_mono,
+    css_mono,
+    avatar_icons: {
+        html,
+        css,
+        jscript,
+        react,
+        node,
+        mongo,
+        graphql,
+        git
+    }
+};
